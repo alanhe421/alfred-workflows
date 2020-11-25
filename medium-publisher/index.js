e4c766f31f0328dd84d7d32be697ede228359087
Alan He <alan@1991421.cn>
2020-11-26 00:21:43 +0800

    update medium-publisher workflow

--- medium-publisher/index.js
+++ medium-publisher/index.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const [, , TOKEN, AUTHOR_ID, FILE] = process.argv;
 
 function main() {
-  const fileData = fs.readFileSync(FILE, 'utf8');
-  const res = extractMeta(fileData);
-  createPost(res);
+    const fileData = fs.readFileSync(FILE, 'utf8');
+    const res = extractMeta(fileData);
+    console.log(res);
 }
 
 /**
@@ -14,21 +14,21 @@ function main() {
  * @returns {{data, canonicalUrl: string, title, tags: *}}
  */
 function extractMeta(data) {
-  const MetaBlockRegex = /^---(\s|\S)*---\n/;
-  const metaData = data.match(MetaBlockRegex)[0];
-  data = data.replace(MetaBlockRegex, '');
-  const title = metaData.match(
-    /(?<=title:\s)[\u4e00-\u9fa5\s\w]*\S(?=\n\S)/
-  )[0];
-  const abbrlink = metaData.match(/(?<=\s{3}-\s)([\u4e00-\u9fa5\s\w]*)(?=\n)/);
-  const tags = metaData.match(/(?<=\s{3}-\s)[\u4e00-\u9fa5\s\w]*(?=\n)/);
-  const createDate = new Date(
-    metaData.match(/(?<=date:\s)\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}/)[0]
-  );
-  const canonicalUrl = `https://1991421.cn/${createDate.getFullYear()}/${
-    createDate.getMonth() + 1
-  }/${createDate.getDate()}/${abbrlink}`;
-  return { data, title, tags, canonicalUrl };
+    const MetaBlockRegex = /^---(\s|\S)*---\n/;
+    const metaData = data.match(MetaBlockRegex)[0];
+    data = data.replace(MetaBlockRegex, '');
+    const title = metaData.match(
+        /(?<=title:\s)[\u4e00-\u9fa5\s\w-]*\S(?=\n\S)/
+    )[0];
+    const abbrlink = metaData.match(/(?<=\s{3}-\s)([\u4e00-\u9fa5\s\w]*)(?=\n)/);
+    const tags = metaData.match(/(?<=\s{3}-\s)[\u4e00-\u9fa5\s\w]*(?=\n)/);
+    const createDate = new Date(
+        metaData.match(/(?<=date:\s)\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}/)[0]
+    );
+    const canonicalUrl = `https://1991421.cn/${createDate.getFullYear()}/${
+        createDate.getMonth() + 1
+    }/${createDate.getDate()}/${abbrlink}`;
+    return {data, title, tags, canonicalUrl};
 }
 
 /**
@@ -36,27 +36,28 @@ function extractMeta(data) {
  * 创建Story
  */
 function createPost(res) {
-  axios
-    .post(
-      `https://api.medium.com/v1/users/${AUTHOR_ID}/posts`,
-      {
-        title: res.title,
-        contentFormat: 'markdown',
-        content: res.data,
-        tags: res.tags,
-        publishStatus: 'draft',
-        canonicalUrl: res.canonicalUrl,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${TOKEN}`,
-        },
-      }
-    )
-    .then(({ data: res }) => {
-      console.log(res.data.url);
-    })
-    .catch(() => {});
+    axios
+        .post(
+            `https://api.medium.com/v1/users/${AUTHOR_ID}/posts`,
+            {
+                title: res.title,
+                contentFormat: 'markdown',
+                content: res.data,
+                tags: res.tags,
+                publishStatus: 'draft',
+                canonicalUrl: res.canonicalUrl,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`,
+                },
+            }
+        )
+        .then(({data: res}) => {
+            console.log(res.data.url);
+        })
+        .catch(() => {
+        });
 }
 
 main();
