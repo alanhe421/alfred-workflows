5598b46cada967815fc4dd36284d3107e7509b76
Alan He <alan@1991421.cn>
2020-11-25 19:16:53 +0800

    init medium

--- medium-publisher/README.md
+++ medium-publisher/README.md
@@ -1,1 +1,15 @@
-> 选中MD文件，一键创建Medium文章
+> 选中MD文件，一键发布到Medium平台
+
+当前解析MD文档中的元数据，面向的是Hexo MD文件。
+
+## 功能
+- 新增Story
+
+## 不支持
+> 受限于Medium API的开放情况，暂不支持以下功能
+
+- 更新Story
+
+
+## 参考文档
+- https://github.com/Medium/medium-api-docs#31-users
--- medium-publisher/index.js
+++ medium-publisher/index.js
@@ -1,26 +1,79 @@
 const axios = require('axios');
-const TOKEN = '2ecfd1899bd5bc26042690614ba57786e6dd9782bd0e847ecc1f5f4e2d3357368';
+const fs = require('fs');
+const [, , TOKEN, AUTHOR_ID, FILE] = process.argv;
 
-async function createPost() {
+function main() {
+    const fileData = fs.readFileSync(FILE, 'utf8');
+    const res = extractMeta(fileData);
+    createPost(res);
+}
 
-    try {
-        const res = await axios.post('https://api.medium.com/v1/users/alanhg/posts', {
-                "title": "Liverpool FC",
-                "contentFormat": "html",
-                "content": "<h1>Liverpool FC</h1><p>You’ll never walk alone.</p>",
-                "canonicalUrl": "http://jamietalbot.com/posts/liverpool-fc",
-                "tags": ["football", "sport", "Liverpool"],
-                "publishStatus": "public"
+/**
+ * @description
+ * UserID不会变
+ */
+function getUserInfo() {
+    axios
+        .get(
+            `https://api.medium.com/v1/me`,
+            {
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`,
+                },
+            }
+        )
+        .then((e) => {
+            console.log(e);
+        })
+        .catch((e) => {
+            console.error(e);
+            console.log(-1);
+        });
+}
+
+/**
+ * 解析MD文件内容，获取元数据
+ * @param data
+ * @returns {{data, canonicalUrl: string, title, tags: *}}
+ */
+function extractMeta(data) {
+    const title = data.match(/(?<=title:\s)\S+(?=\n)/)[0];
+    const abbrlink = data.match(/(?<=abbrlink:\s)(\s|\S)*(?=date)/)[0];
+    const tags = data.match(/(?<=tags:\n)(\s|\S)*(?=(abbrlink|---))/)[0].match(/(?<=-\s)\S+(?=(\n|$))/g);
+    const createDate = new Date(data.match(/(?<=date:\s)(\s|\S)*(?=\n---)/)[0]);
+    const canonicalUrl = `https://1991421.cn/${createDate.getFullYear()}/${createDate.getMonth() + 1}/${createDate.getDate()}/${abbrlink}`;
+    data = data.replace(/^---(\s|\S)*---\n/, '')
+    return {data, title, tags, canonicalUrl};
+}
+
+/**
+ * @description
+ * 创建Story
+ */
+function createPost(res) {
+    axios
+        .post(
+            `https://api.medium.com/v1/users/${AUTHOR_ID}/posts`,
+            {
+                title: res.title,
+                contentFormat: 'markdown',
+                content: res.data,
+                tags: res.tags,
+                publishStatus: 'draft',
+                canonicalUrl: res.canonicalUrl
             },
             {
                 headers: {
-                    'Authorization': `Bearer ${TOKEN}`
-                }
-            })
-        console.log(res);
-    } catch (e) {
-        console.error(e);
-    }
+                    Authorization: `Bearer ${TOKEN}`,
+                },
+            }
+        )
+        .then((e) => {
+            console.log(0);
+        })
+        .catch((e) => {
+            console.log(-1);
+        });
 }
 
-createPost();
+main();
