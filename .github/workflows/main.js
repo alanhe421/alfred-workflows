5304adc2aece36a97c2734224a1bd333cab7d080
alanhe <qianghe421@gmail.com>
2023-09-10 16:11:02 +0800

    revert ci action

--- .github/workflows/main.js
+++ .github/workflows/main.js
@@ -142,27 +142,6 @@ function buildBadgeContent({version}, folderName, filename) {
 `
 }
 
-/**
- * 保存workflow源码文件，方便PR对比
- * @param workflowFolder
- * @param workflow
- * @returns {{plistObj: *}}
- */
-function parseWorkflowInfo(workflowFolder, workflow) {
-  const workFlowFile = workflowFolder + '/' + workflow;
-  const zip_suffix = '.zip';
-  const workFlowZipFile = workflowFolder + '/src' + zip_suffix
-  fs.copyFileSync(workFlowFile, workFlowZipFile);
-  // 解压zip文件，创建文件夹
-  const workFlowUnzipFolder = workFlowZipFile.substring(0, workFlowZipFile.length - zip_suffix.length);
-  execSync(`unzip -o ${workFlowZipFile} -d ${workFlowUnzipFolder}`);
-  execSync(`rm -rf ${workFlowZipFile}`);
-  // 源码中node_modules不纳入版本管理
-  execSync(`rm -rf ${workFlowUnzipFolder}/node_modules`);
-  const plistObj = plist.parse(fs.readFileSync(`${workFlowUnzipFolder}/info.plist`, 'utf8'));
-  return {plistObj};
-}
-
 function updatePerWorkflowPage() {
   const targetFolder = path.resolve(__dirname, '../../');
   const folders = fs.readdirSync(targetFolder);
