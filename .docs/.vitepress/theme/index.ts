0d20d2411c8b83a727cd0536a809fb239ff2ebf1
Alan He <qianghe421@gmail.com>
2024-11-05 17:28:30 +0800

    docs: support view image

--- .docs/.vitepress/theme/index.ts
+++ .docs/.vitepress/theme/index.ts
@@ -4,6 +4,10 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import HomeUnderline from './components/HomeUnderline.vue'
+import 'viewerjs/dist/viewer.min.css';
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import { useRoute } from 'vitepress';
 
 export default {
   extends: DefaultTheme,
@@ -15,6 +19,10 @@ export default {
   enhanceApp({ app, router, siteData }) {
     // ...
     app.component('HomeUnderline' , HomeUnderline) // 首页下划线
-
+    app.component('vImageViewer', vImageViewer);
+  },
+  setup() {
+    const route = useRoute();
+    imageViewer(route);
   }
 } satisfies Theme
